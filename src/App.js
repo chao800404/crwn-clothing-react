25f74143834e5d0e799f659dcf803d13830f4285
chao <a0921342997@gmail.com>
2022-02-21 05:50:06 +0800

    updating our app with a shop page, collection and collection component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 /** @format */
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
 import Hompage from "./page/homepage/homepage.component";
+import ShopPage from "./page/shop/shop.components";
 
 function App() {
   return (
     <div>
-      <Hompage />
+      <Switch>
+        <Route exact path="/" component={Hompage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
